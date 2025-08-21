970bf0981eb68126c8b8eb28a9211a7c4b023f5e
ChSunayReddy <sunayreddyc@gmail.com>
2025-08-21 14:06:27 +0530

    Time: 1 ms (13.17%), Space: 41.4 MB (82.01%) - LeetHub

---
+++ 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.java
@@ -0,0 +1,10 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        Set<Character> set=new HashSet<>();
+        for(char i:s.toCharArray()){
+            if(set.contains(i)) return i;
+            set.add(i);
+        }
+        return 'a';
+    }
+}
