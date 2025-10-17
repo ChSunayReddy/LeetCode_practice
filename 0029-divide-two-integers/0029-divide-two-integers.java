8c7c167c81597e8ea5c9d128dce1f0b0801f2f90
ChSunayReddy <sunayreddyc@gmail.com>
2025-10-17 17:24:25 +0530

    Time: 1 ms (76.15%), Space: 40.9 MB (77.22%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int divide(int n, int d) {
+        if(n==-2147483648 && d==-1) return 2147483647;
+        if(n<0 && d<0) return (int)n/d;
+        else return (int)n/d;
+    }
+}
