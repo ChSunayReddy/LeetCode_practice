c2e6714932c8755f4a63ad334342583be398421f
ChSunayReddy <sunayreddyc@gmail.com>
2025-10-29 11:16:16 +0530

    Time: 1 ms (100%), Space: 46.4 MB (7.81%) - LeetHub

---
+++ 0800-letter-case-permutation/0800-letter-case-permutation.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<String> letterCasePermutation(String s) {
+        List<String> res= new ArrayList<>();
+        backtrack(new StringBuilder(s),0,res);
+        return res;
+    }
+    public void backtrack(StringBuilder sb,int i, List<String> res){
+        if(i==sb.length()){
+            res.add(sb.toString());
+            return;
+        }
+        if(Character.isDigit(sb.charAt(i))){
+            backtrack(sb,i+1,res);
+            return;
+        }
+        sb.setCharAt(i,Character.toLowerCase(sb.charAt(i)));
+        backtrack(sb,i+1,res);
+        sb.setCharAt(i,Character.toUpperCase(sb.charAt(i)));
+        backtrack(sb,i+1,res);
+    }
+}
