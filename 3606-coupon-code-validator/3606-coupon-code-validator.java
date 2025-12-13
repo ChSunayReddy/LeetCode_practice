2fc1e0137c3ddbf9fba05614b62db22d040c25de
ChSunayReddy <sunayreddyc@gmail.com>
2025-12-13 11:21:51 +0530

    Time: 5 ms (97.27%), Space: 47.3 MB (57.73%) - LeetHub

--- 3606-coupon-code-validator/3606-coupon-code-validator.java
+++ 3606-coupon-code-validator/3606-coupon-code-validator.java
@@ -1,32 +1,47 @@
 class Solution {
     public List<String> validateCoupons(String[] code, String[] businessLine, boolean[] isActive) {
-        final List<String> BUSINESS_LINE_ORDER = 
-        Arrays.asList("electronics", "grocery", "pharmacy", "restaurant");
-        int n = code.length;
-        List<Coupon> ans = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            String c = code[i];
-            String bl = businessLine[i];
-            boolean ia = isActive[i];
-            if(!c.isEmpty() && c.matches("[a-zA-Z0-9_ ]*") && ia && !bl.equals("invalid")){
-                if(BUSINESS_LINE_ORDER.contains(bl)) ans.add(new Coupon(c,bl));
+        List<String> electronics = new ArrayList<>();
+        List<String> grocery = new ArrayList<>();
+        List<String> pharmacy = new ArrayList<>();
+        List<String> restaurant = new ArrayList<>();
+        
+        for (int i = 0; i < code.length; i++) {
+            if (!isActive[i] || code[i].isEmpty()) continue;
+            
+            boolean validCode = true;
+            for (char c : code[i].toCharArray()) {
+                if (!Character.isLetterOrDigit(c) && c != '_') {
+                    validCode = false;
+                    break;
+                }
+            }
+            if (validCode) {
+                if (businessLine[i].equals("electronics")) {
+                    electronics.add(code[i]);
+                }
+                else if (businessLine[i].equals("grocery")){
+                    grocery.add(code[i]);
+                }
+                else if (businessLine[i].equals("pharmacy")){
+                    pharmacy.add(code[i]);
+                }
+                else if (businessLine[i].equals("restaurant")){
+                    restaurant.add(code[i]);
+                }
             }
         }
-        ans.sort((a,b) -> {
-            int id1 = BUSINESS_LINE_ORDER.indexOf(a.businessLine);
-            int id2 = BUSINESS_LINE_ORDER.indexOf(b.businessLine);
-            if(id1!=id2) return Integer.compare(id1,id2);
-            else return a.code.compareTo(b.code);
-        });
-        return ans.stream().map(coupon -> coupon.code).collect(Collectors.toList());
-    }
-    private static class Coupon {
-        String code;
-        String businessLine;
         
-        Coupon(String code, String businessLine) {
-            this.code = code;
-            this.businessLine = businessLine;
-        }
+        Collections.sort(electronics);
+        Collections.sort(grocery);
+        Collections.sort(pharmacy);
+        Collections.sort(restaurant);
+        
+        List<String> result = new ArrayList<>();
+        result.addAll(electronics);
+        result.addAll(grocery);
+        result.addAll(pharmacy);
+        result.addAll(restaurant);
+        
+        return result;
     }
 }
