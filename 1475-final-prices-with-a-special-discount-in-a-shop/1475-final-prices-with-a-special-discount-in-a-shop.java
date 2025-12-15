41c8d83aa98b7b02db0e53dd520074952688e75c
ChSunayReddy <sunayreddyc@gmail.com>
2025-12-15 11:49:32 +0530

    Time: 2 ms (71.33%), Space: 45.1 MB (74.43%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        int[] ans = new int[n];
+        for(int i=0;i<n;i++){
+            boolean c = false; 
+            if(i==n-1) ans[i] = prices[i];
+            for(int j=i+1;j<n;j++){
+                if(j>i && prices[j]<=prices[i]){
+                    ans[i] = prices[i] - prices[j];
+                    c= true;
+                    break;
+                }
+                if(!c) ans[i] = prices[i];
+            }
+        }
+        return ans;
+    }
+}
