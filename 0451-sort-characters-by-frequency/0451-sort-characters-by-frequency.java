f2424490a11708043d5feb10b2656423350c9dca
Ch Sunay reddy <sunayreddyc@gmail.com>
2026-02-04 12:45:08 +0530

    Time: 5 ms (95.93%), Space: 46.5 MB (86.78%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,16 +1,32 @@
 class Solution {
     public String frequencySort(String s) {
-        HashMap<Character, Integer> hm = new HashMap<>();
-        for(char ch:s.toCharArray()){
-            hm.put(ch, hm.getOrDefault(ch,0)+1);
-        } 
-        List<Map.Entry<Character, Integer>> l = new ArrayList<>(hm.entrySet());
-        // Sorting
-        l.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
-        //store
+         // 1. ASCII frequency array
+        int[] freq = new int[128];
+        for (char c : s.toCharArray()) {
+            freq[c]++;
+        }
+
+        // 2. Buckets by frequency
+        List<Character>[] buckets = new List[s.length() + 1];
+        for (int i = 0; i < 128; i++) {
+            int f = freq[i];
+            if (f > 0) {
+                if (buckets[f] == null)
+                    buckets[f] = new ArrayList<>();
+                buckets[f].add((char) i);
+            }
+        }
+
+        // 3. Build output
         StringBuilder sb = new StringBuilder();
-        for(Map.Entry<Character, Integer> i : l){
-            for(int j=0;j<i.getValue();j++) sb.append(i.getKey());
+        for (int i = buckets.length - 1; i > 0; i--) {
+            if (buckets[i] != null) {
+                for (char c : buckets[i]) {
+                    for (int j = 0; j < i; j++) {
+                        sb.append(c);
+                    }
+                }
+            }
         }
         return sb.toString();
     }
