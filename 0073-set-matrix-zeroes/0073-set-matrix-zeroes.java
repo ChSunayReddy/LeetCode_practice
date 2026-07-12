7f96b7cdd650791174ccb8a2882a81ad49a35a72
Ch Sunay reddy <sunayreddyc@gmail.com>
2026-07-12 23:52:57 +0530

    Time: 0 ms (100%), Space: 47.6 MB (43.61%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,24 +1,30 @@
 class Solution {
-    public void setZeroes(int[][] arr) {
-        int m = arr.length, n = arr[0].length;
-        ArrayList<Integer> rows = new ArrayList<>();
-        ArrayList<Integer> cols = new ArrayList<>();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(arr[i][j]==0){
-                    rows.add(i);
-                    cols.add(j);
+    public void setZeroes(int[][] matrix) {
+        int row = matrix.length, col = matrix[0].length;
+        int rowarr[] = new int[row];
+        int colarr[] = new int[col];
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                if(matrix[i][j] == 0) {
+                    rowarr[i] = -1;
+                    colarr[j] = -1;
                 }
             }
         }
-        for(int i:rows){
-            for(int j=0;j<n;j++){
-                arr[i][j] = 0;
+
+        for(int i=0; i<row; i++) {
+            if(rowarr[i] == -1) {
+                for(int j=0; j<col; j++) {
+                    matrix[i][j] = 0;
+                }
             }
         }
-        for(int i:cols){
-            for(int j=0;j<m;j++){
-                arr[j][i] = 0;
+
+        for(int j=0; j<col; j++) {
+            if(colarr[j] == -1) {
+                for(int i=0; i<row; i++) {
+                    matrix[i][j] = 0;
+                }
             }
         }
     }
