9702a1f71c964c2bf91e54888fd7c3d8d468e26b
Ch Sunay reddy <sunayreddyc@gmail.com>
2026-09-22 19:13:33 +0530

    Time: 2 ms (99.17%), Space: 122.7 MB (22.27%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,18 +1,20 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        int n = nums.length;
-        if(n<3) return false;
+        if (nums == null || nums.length < 3) return false;
+
         int first = Integer.MAX_VALUE;
         int second = Integer.MAX_VALUE;
-        for(int x:nums){
-            if(x > second) return true;
-            else if(x > first){
-                second = x;
-            }
-            else if(first > x){
-                first = x;
+
+        for (int num : nums) {
+            if (num <= first) {
+                first = num;           // found a new smallest "start"
+            } else if (num <= second) {
+                second = num;          // found a new smallest valid "middle"
+            } else {
+                return true;           // num > second > first → triplet found!
             }
         }
+
         return false;
     }
 }
